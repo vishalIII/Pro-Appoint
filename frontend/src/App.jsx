18fa886b3f583ac11e722f2b512c66015a3f0982
Anand Vishal <anandkadagiak37@gmail.com>
2026-02-19 00:05:08 +0530

    payment button added in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,24 @@
 import AppRoutes from "./routes/AppRoutes";
-
+import { useEffect } from "react";
 function App() {
+   useEffect(() => {
+    // Prevent loading script multiple times
+    if (window.Razorpay) return;
+
+    const script = document.createElement("script");
+    script.src = "https://checkout.razorpay.com/v1/checkout.js";
+    script.async = true;
+
+    script.onload = () => {
+      console.log("Razorpay script loaded");
+    };
+
+    script.onerror = () => {
+      console.error("Failed to load Razorpay script");
+    };
+
+    document.body.appendChild(script);
+  }, []);
   return (
     <div>
       <AppRoutes />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,37 @@
+import axios from "axios";
+
 export default function Home() {
-  return <h1>Home Page</h1>;
-}
+
+
+  const payNow = async () => {
+    try {
+      const { data: order } = await axios.post(`http://localhost:5000/api/payment/create-order`, { amount: 500 });
+
+      const options = {
+        key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+        amount: order.amount,
+        currency: "INR",
+        name: "My SaaS App",
+        description: "One-time payment",
+        order_id: order.id,
+
+        handler: async (response) => {
+          const { data } = await axios.post(`http://localhost:5000/api/payment/verify-payment`, response);
+          if (data.success) alert("Payment successful");
+          else alert("Payment verification failed");
+        }
+      };
+
+      const rzp = new window.Razorpay(options);
+      rzp.open();
+    } catch (err) {
+      console.error(err);
+      alert("Payment failed");
+    }
+  };
+
+  return <button onClick={payNow}>Pay ₹499</button>;
+};
+
+
+
