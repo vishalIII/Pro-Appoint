9f37ff29f318d914e521d0a1deafafec10228c11
Vishal <vishalmachchha1310@gmail.com>
2026-05-02 21:40:47 +0000

    added CI-CD

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 
 /* -------- Middleware -------- */
 app.use(cors({
-  origin:['http://localhost:5173', 'http://127.0.0.1:5173'], // 🔥 FIX
+  origin:['http://localhost:5173', 'http://127.0.0.1:5173'],
   credentials: true
 }));
 app.use(express.json());
